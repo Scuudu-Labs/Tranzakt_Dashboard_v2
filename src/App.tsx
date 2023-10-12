158baa0c851b2a4f7469621067c9310fad7a4ebc
Ifenna-Monanu <i.monanu@genesystechhub.com>
2023-10-12 02:31:16 +0100

    fix: config setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import DashboardPage from "./pages/Dashboard";
 import ManageUserPage from "./pages/ManageUser";
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="font-montserrat">
