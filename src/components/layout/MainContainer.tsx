e187547d020626763c199a9e94e8f09caae4a803
Ifenna-Monanu <i.monanu@genesystechhub.com>
2023-10-18 11:54:13 +0100

    fix:implemened bar charts and dro-pdowns

--- src/components/layout/MainContainer.tsx
+++ src/components/layout/MainContainer.tsx
@@ -4,11 +4,11 @@ import TopNavbar from "../nav/TopNav";
 
 export default function MainContainer(props: { children: ReactNode }) {
   return (
-    <div className="h-screen w-screen">
+    <div className="">
       <TopNavbar />
-      <div className="h-[88%] flex w-full overflow-x-scroll">
+      <div className="flex w-full">
         <LeftNavbar />
-        <div className="h-full bg-[#EEEEEE] w-[85%] pt-6 px-5">
+        <div className="min-h-screen ml-[210px] bg-[#EEEEEE] w-full pt-6 px-7">
           {props.children}
         </div>
       </div>
