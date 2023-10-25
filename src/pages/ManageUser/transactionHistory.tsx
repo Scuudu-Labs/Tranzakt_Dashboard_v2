eda79ddcb503f9dad1cd3272e9165dd45fd5ce80
Ifenna-Monanu <i.monanu@genesystechhub.com>
2023-10-25 12:18:44 +0100

    fix/moved transaction to a seprate file

--- src/pages/ManageUser/transactionHistory.tsx
+++ src/pages/ManageUser/transactionHistory.tsx
@@ -1,14 +1,4 @@
-import { ReactComponent as Credit} from "../../assets/icons/credit.svg";
-import { ReactComponent as Debit} from "../../assets/icons/debit.svg";
-
-
-interface ITransact {
-    type: string;
-    text: string
-    details: string
-    amount: string
-    date: string
-}
+import Transaction from "./Transaction"
 
 const transactions = [
     {
@@ -42,23 +32,7 @@ const transactions = [
     
 ]
 
-const EachTransaction = ({type, text, details, amount, date}:ITransact ) => {
-    return(
-        <div className='py-4 px-4 flex items-center justify-between border-b last:border-none border-[#E3E3E3]'>
-            <div className='flex items-center'>
-                {type === "credit" ? <Credit /> : <Debit /> }
-                <div className='flex flex-col ml-3'>
-                    <span className='text-[12px] font-montserrat text-[#3F3F3F] tracking-[0.3px] font-[500]'>{text}</span>
-                    <span className='text-[#A1A1A1] py-1 uppercase text-[12px] font-montserrat tracking-[0.3px] font-[500]'>{details}</span>
-                </div>
-            </div>
-            <div className='flex flex-col text-right'>
-                <span className={`${type === "credit" ? "text-[#32C87D]" : "text-[#FF2636]"} text-[16px] tracking-[0.4px] font-montserrat `}>{amount}</span>
-                <span className='text-[#A1A1A1] py-1 uppercase text-[12px] font-montserrat tracking-[0.3px] font-[400]'>{date}</span>
-            </div>
-        </div>
-    )
-}
+
 
 const TransactionHistory = () => {
   return (
@@ -67,7 +41,7 @@ const TransactionHistory = () => {
         <div className='flex flex-col'>
             {
                 transactions.map((transaction, index) => (
-                    <EachTransaction key={index} type={transaction.type} text={transaction.text} amount={transaction.amount} details={transaction.details} date={transaction.date}/>
+                    <Transaction key={index} type={transaction.type} text={transaction.text} amount={transaction.amount} details={transaction.details} date={transaction.date}/>
                 ))
             }
         </div>
