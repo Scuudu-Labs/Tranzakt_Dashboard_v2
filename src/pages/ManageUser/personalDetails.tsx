05129ccebd853252dc0e1cbecb985e2ba8c396ee
Ifenna-Monanu <137804212+Ifenna-Monanu@users.noreply.github.com>
2023-11-01 15:35:09 +0100

    fix: fixed the graph ui bugs (#7)
    * fix: fixed the graph ui bugs
    
    * fix: fixed the graph ui bugs
    
    * fix: resolved build errrors
    
    * fix: resolved build errors changed file naming

--- src/pages/ManageUser/personalDetails.tsx
+++ src/pages/ManageUser/personalDetails.tsx
@@ -1,7 +1,7 @@
 const DisplayBox = ({ title, value }: { title: string; value: string }) => {
   return (
-    <div className="w-full flex flex-col px-[16px] py-[8px]  border-[0.5px] border-[#A1A1A1] rounded-[8px]">
-      <span className="text-[#3F3F3F] text-[12px] capitalize font-montserrat pb-3">
+    <div className="w-full flex flex-col px-[16px] py-[10px] h-[70px]  border-[0.5px] border-[#A1A1A1] rounded-[8px]">
+      <span className="text-[#3F3F3F] text-[12px] capitalize font-montserrat pb-1">
         {title}
       </span>
       <h2 className="text-[#272626] text-[16px] tracking-[0.3px] font-montserrat font-[500] ">
@@ -13,20 +13,33 @@ const DisplayBox = ({ title, value }: { title: string; value: string }) => {
 
 const PersonalDetails = () => {
   return (
-    <div className="w-full rounded-[8px] py-4 h-[450px] bg-white  border-[#EAEAEA] border shadow-sm">
+    <div className="w-full rounded-[8px] py-4  bg-white  border-[#EAEAEA] border shadow-sm">
       <h1 className="text-[#3F3F3F] px-4 text-[18px]  font-montserrat mb-3">
         Personal Details
       </h1>
-      <div className="grid grid-rows-5 px-4 grid-cols-2 gap-6">
-        <DisplayBox title="first name" value="Alessa" />
-        <DisplayBox title="first name" value="Alessa" />
-        <DisplayBox title="first name" value="Alessa" />
-        <DisplayBox title="first name" value="Alessa" />
-        <div className="col-span-2">
+      <div className="h-[380px] overflow-auto">
+        <div className="grid grid-rows-5  overflow-y-auto  px-4 grid-cols-2 gap-4">
           <DisplayBox title="first name" value="Alessa" />
+          <DisplayBox title="lirst name" value="Abubakar" />
+          <DisplayBox title="DOB" value="2/02/2002" />
+          <DisplayBox title="Gender" value="Female" />
+          <div className="col-span-2">
+            <DisplayBox title="Email" value="alessaabubakar@gmail.com" />
+          </div>
+          <DisplayBox title="Phone Number" value="+2349012345678" />
+          <DisplayBox title="User ID" value="#1234" />
+          <div className="col-span-2">
+            <DisplayBox title="Occupation" value="Cooperate Worker" />
+          </div>
+          <div className="col-span-2">
+            <button
+              type="submit"
+              className="text-white bg-[#32C87D] w-full mx-auto py-3 mb-2 mt-2 rounded-md"
+            >
+              Activate
+            </button>
+          </div>
         </div>
-        <DisplayBox title="first name" value="Alessa" />
-        <DisplayBox title="first name" value="Alessa" />
       </div>
     </div>
   );
